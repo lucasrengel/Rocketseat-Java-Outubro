40c0ae14a1011c253ba9e8c86eff5e2f7c4d1973
lucasrengel <lucaslangel@hotmail.com>
2023-10-09 18:43:38 -0300

    primeira aula concluida

---
+++ src/main/java/br/com/lucasrengel/todolist/user/UserController.java
@@ -0,0 +1,35 @@
+package br.com.lucasrengel.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /**
+     * String (texto)
+     * Integer (int)
+     * Double (double)
+     * Float (float)
+     * Char (A C )
+     * Date (data)
+     * void
+     */
+    /**
+     * Body
+     *
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.println(userModel.name);
+    }
+}
