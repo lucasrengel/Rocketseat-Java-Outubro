b93a34201afa3df4bc7837252a59d191ba7f1357
lucasrengel <lucaslangel@hotmail.com>
2023-10-12 13:41:06 -0300

    segunda aula concluida aprendizado: integracao em um banco de dados em memoria

--- src/main/java/br/com/lucasrengel/todolist/user/UserController.java
+++ src/main/java/br/com/lucasrengel/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.lucasrengel.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /**
-     * String (texto)
-     * Integer (int)
-     * Double (double)
-     * Float (float)
-     * Char (A C )
-     * Date (data)
-     * void
-     */
-    /**
-     * Body
-     *
-     */
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null){
+            return ResponseEntity.status(400).body("Usuario ja existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(200).body(userCreated);
     }
 }
--- src/main/java/br/com/lucasrengel/todolist/user/UserModel.java
+++ src/main/java/br/com/lucasrengel/todolist/user/UserModel.java
@@ -1,11 +1,29 @@
 package br.com.lucasrengel.todolist.user;
 
+import jakarta.persistence.Column;
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     public String username;
     public String name;
     public String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
